55a8fb06a6845eaae705d5fc1f10f33c807dd2fc
SamuelRamirezM <sramirezmu@unal.edu.co>
2024-02-26 22:27:06 -0500

    Update control.py

--- televisores/control.py
+++ televisores/control.py
@@ -1,34 +1,34 @@
 class Control():
-    def turnOn(self) -> None:
-        self._tv.turnOff()
-
-    def turnOff(self) -> None:
-        self._tv.turnOn()
-
-    def canalUp(self) -> None:
-        self._tv.canalUp()
-
-    def canalDown(self) -> None:
-        self._tv.canalDown()
-    
-    def volumenUp(self) -> None:
-        self._tv.volumenUp()
-    
-    def volumenDown(self) -> None:
-        self._tv.volumenDown()
-
-    def setCanal(self, canal) -> None:
-        self._tv.setCanal(canal)
-    
-    def setVolumen(self, volumen) -> None:
-        self._tv.setCanal(volumen)
-
     def enlazar(self, tv):
         self._tv = tv
         tv.setControl(self)
+
+    def turnOn(self):
+        self._tv.turnOff()
+
+    def turnOff(self):
+        self._tv.turnOn()
+
+    def canalUp(self):
+        self._tv.canalUp()
+
+    def canalDown(self):
+        self._tv.canalDown()
+    
+    def volumenUp(self):
+        self._tv.volumenUp()
+    
+    def volumenDown(self):
+        self._tv.volumenDown()
+
+    def setCanal(self, canal):
+        self._tv.setCanal(canal)
+    
+    def setVolumen(self, volumen):
+        self._tv.setCanal(volumen)
 
     def getTv(self):
         return self._tv
     
     def setTv(self, tv):
-        self._tv.setControl(self)
+        self._tv = tv
